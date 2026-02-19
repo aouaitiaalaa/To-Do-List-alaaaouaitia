c1af22fb44f8bb585efa6f679877b4cd5b82cd54
Alaa Aouaitia <alaaaouaitiaha@gmail.com>
2026-02-19 17:51:07 +0100

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+let input = document.getElementById("input");
+let btn = document.getElementById("btn");
+let list = document.getElementById("list");
+let counter = document.getElementById("counter");
+let clearBtn = document.getElementById("clear");
+let tasks =JSON.parse(localStorage.getItem("tasks")) || [];
 
+function createTask(text,done=false){
+let li =document.createElement("li");
+  li.innerText= text;
+  if(done){
+li.classList.add("done");
+  }
+  li.addEventlistener("click",function(){
+   li.classList.toggle("done");
+    saveTasks();
+  });
+}
